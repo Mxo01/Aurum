f085b07db1acf2798d40c919ea880dc146b11551
Mxo01 <mariodimodica.01@gmail.com>
2026-03-12 19:06:21 +0100

    feat: remove cache

--- aurum-be/src/main/java/com/backend/aurum/AurumApplication.java
+++ aurum-be/src/main/java/com/backend/aurum/AurumApplication.java
@@ -2,10 +2,8 @@ package com.backend.aurum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class AurumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AurumApplication.class, args);
--- aurum-be/src/main/java/com/backend/aurum/domain/user/service/UserService.java
+++ aurum-be/src/main/java/com/backend/aurum/domain/user/service/UserService.java
@@ -4,7 +4,6 @@ import com.backend.aurum.domain.user.model.User;
 import com.backend.aurum.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +14,6 @@ public class UserService {
 	private final UserRepository userRepository;
 
 	@Transactional
-	@Cacheable(value = "users", key = "#jwtId")
 	public User findOrCreate(String jwtId, String email) {
 		return userRepository
 				.findByJwtId(jwtId)
